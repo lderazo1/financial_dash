b625be4bfecd89f56a18b1383a358c78dc07f079
lderazo1 <luiyi.darmijos@gmail.com>
2022-03-05 23:10:27 -0500

    Fix

--- app.py
+++ app.py
@@ -23,8 +23,9 @@ app = Dash(
     external_stylesheets=['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css']
 )
 app.title = "Reportes Financieros"
-app.config.suppress_callback_exceptions=True
 server = app.server
+app.config.suppress_callback_exceptions=True
+
 
 # Describe the layout/ UI of the app
 app.layout = html.Div(
